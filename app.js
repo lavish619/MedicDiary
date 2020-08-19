dca36c9871319156fec5809346249001df3e1b64
Lavish Bansal <lavish.bansal.mat19@iitbhu.ac.in>
2020-08-20 01:00:04 +0530

    gitignore added and style,mainpage added to project

--- app.js
+++
@@ -1,9 +0,0 @@
-const hero= document.querySelector(".hero")
-const slider= document.querySelector(".slider")
-const headline= document.querySelector(".headline")
-
-const tl= new TimelineMax();
-
-tl.fromTo(hero, 1, {height: "0%"}, {height: "100%", ease: Power2.easeInOut})
-.fromTo(hero,1.2, {width:"100%"}, {width:"80%",ease: Power2.easeInOut})
-.fromTo(slider,1,{x:"-100%"}, {x:"0%", ease: Power2.easeInOut},"-=1");
