71f440e95e659c9f1d991c062074b4c57e5d16b9
harsh0015 <harshdeepsingh0015@gmail.com>
2020-09-10 12:41:22 +0530

    added auth

--- MedicDiary/doctor/urls.py
+++ MedicDiary/doctor/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 app_name = 'doctor'
 urlpatterns = [
+      path('registerpage/',views.registerpage,name ='registerpage'),
+       path('signup/',views.signup, name ='signup'),
+         path('logind/',views.loginn, name ='logind'),
     # path('doclogin/',views.doclogin,name ='doclogin'),
     # path('docregister/',views.docregister,name ='docregister')
 ]
--- MedicDiary/doctor/views.py
+++ MedicDiary/doctor/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
+
+def registerpage(request):
+    return render(request,'doctor/register.html')
+
+
+def signup(request):
+    if request.method=="POST":
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        # age=request.POST['age']
+        # bgroup=request.POST['bgroup']
+        # dob=request.POST['dob']
+        # gender=request.POST['gender']
+        # address=request.POST['address']
+        username=request.POST['username']
+        password=request.POST['password']
+        email=request.POST['email']
+        # mobile=request.POST['mobile']
+        # usertype="patient"
+
+
+        if len(username)>15:
+            messages.error(request,'length of username should be less than15')
+            return redirect('patient:registerpage')
+        
+        # new_patient=patient_details()
+        # new_patient.fname=fname
+        # new_patient.lame=lname
+        # new_patient.age=age
+        # new_patient.bgroup=bgroup
+        # new_patient.dob=dob
+        # new_patient.gender=gender
+        # new_patient.address=address
+        # new_patient.username=username
+        # new_patient.email=email
+        # new_patient.mobile=mobile
+
+        # new_patient.save()
+
+        myuser=User.objects.create_user(username,email,password)
+        myuser.first_name=fname
+        myuser.last_name=lname
+        # myuser.age=age
+        # myuser.bgroup=bgroup
+        # myuser.dob=dob
+        # myuser.gender=gender
+        # myuser.address=address
+        # myuser.mobile=mobile
+       
+        
+        myuser.save()
+
+        messages.success(request, 'Form submission successful')
+        return redirect('/')
+       
+
+    else :
+        return HttpResponse('404-not found')
+
+
+def loginn(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            print("innn")
+            return HttpResponse('loggedin as doctor') 
+
+        else :
+            print("invalid credentials")
+            return render(request,'centralapp/mainpage.html')
+
+
+        return HttpResponse('login')
+
+
+
+    else:
+        return HttpResponse('404-not found')
+
