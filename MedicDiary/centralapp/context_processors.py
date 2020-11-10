304de4fc9f36c069189ac79c8e7873b0da28946d
madhav <madhav.mittal.mat19@itbhu.ac.in>
2020-11-10 11:05:37 +0530

    navbar my profile retracted

--- MedicDiary/centralapp/context_processors.py
+++ MedicDiary/centralapp/context_processors.py
@@ -1,20 +1,29 @@
-from patient.models import PatientProfile
-from doctor.models import DoctorProfile
-
-def user_type(request):
-
-    if request.user.is_authenticated:
-        if PatientProfile.objects.filter(patient = request.user):
-            isuser = PatientProfile.objects.filter(patient = request.user)
-        elif DoctorProfile.objects.filter(doctor=request.user):
-            isuser = DoctorProfile.objects.filter(doctor=request.user)
-        usertype = [int(each.usertype) for each in isuser][0]
-
-        return {
-            'usertype' : usertype
-        }
-    else:
-        x=3
-        return {
-            'x' : x
-        }
+# from patient.models import PatientProfile
+# from doctor.models import DoctorProfile
+# def user_type(request):
+#
+#     if request.user.is_authenticated:
+#         # if PatientProfile.objects.filter(patient = request.user):
+#         #     isuser = PatientProfile.objects.filter(patient = request.user)
+#         # elif DoctorProfile.objects.filter(doctor=request.user):
+#         #     isuser = DoctorProfile.objects.filter(doctor=request.user)
+#         # usertype = [int(each.usertype) for each in isuser][0]
+#         # if request.user.patient.usertype=="1":
+#         if PatientProfile.objects.filter(patient = request.user):
+#             isuser = PatientProfile.objects.filter(patient = request.user)
+#             # usert = [int(each.usertype) for each in isuser][0]
+#         elif DoctorProfile.objects.filter(doctor=request.user):
+#             isuser = DoctorProfile.objects.filter(doctor=request.user)
+#         usert = [int(each.usertype) for each in isuser][0]
+#
+#         return {
+#             'usert' : usert
+#         }
+#     else:
+#         x=3
+#         return {
+#             'x' : x
+#         }
+#     # except:
+#         # return {
+#         # }
